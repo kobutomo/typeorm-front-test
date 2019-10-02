93cb0b45a7932ad65bd1c787b5bfa22cad492075
ntomoro <n.tomoro13@gmail.com>
2019-10-02 19:50:26 +0900

    いろいろ

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "axios"
 
 const App: React.FC = () => {
@@ -8,7 +8,20 @@ const App: React.FC = () => {
     password: string
     delete: boolean
   }
-  const initialState: User[] = []
+
+  type Post = {
+    title: string;
+    content: string;
+    images: string[];
+    rating: number;
+    tags: string[];
+    location: string;
+    location_search: string;
+    createdAt?: Date
+    updatedAt?: Date
+    delete: boolean
+  }
+  const initialState: Post[] = []
   const [state, setState] = useState(initialState)
 
   type LoginState = {
@@ -17,29 +30,43 @@ const App: React.FC = () => {
   }
   const [loginState, setLoginState] = useState<LoginState>({ email: "", password: "" })
 
-  const fetchUsers = async () => {
-    const users = await axios.get<User[]>("/api/read").catch(err => console.log(err))
-    if (users) {
-      setState(users.data)
-    }
+  const login = async () => {
+    const result = await axios.post("/api/login", { email: loginState.email, password: loginState.password })
+    console.log(result.data)
+    localStorage.setItem("token", result.data.token)
   }
   const addUser = async () => {
     let params = new URLSearchParams()
     params.append("email", loginState.email)
     params.append("password", loginState.password)
-    await axios.post("/api/create/", params).then(res => { console.log(res) }).catch(err => console.log(err))
+    await axios.post("/api/create/", params).then(res => { console.log(res.data) }).catch(err => console.log(err))
   }
-  const mapUsers = () => {
-    const userList = state.map(user => {
-      return (
-        <div>
-          {user.id}
-          <br />
-          {user.login}
-        </div>
-      )
-    })
-    return userList
+  const post = async () => {
+    const detail = {
+      title: "投稿" + (Math.random() * 10).toString().slice(0, 1),
+      content: "説明説明",
+      images: [""],
+      rating: 3,
+      tags: [""],
+      location: "大阪",
+      locationSearch: "梅田"
+    }
+    const resuponse = await axios.post("/api/post/", detail, { headers: { "x-access-token": localStorage.token } })
+    console.log(resuponse.data)
+    mapUsers()
+  }
+  
+  useEffect(() => {
+  },[])
+  
+  const del = async () => {
+    await axios.get("/api/delete/", { headers: { "x-access-token": localStorage.token } })
+    mapUsers()
+  }
+  const mapUsers = async () => {
+    const posts = await axios.get<Post[]>("/api/post/")
+    console.log(posts.data)
+    setState(posts.data)
   }
   return (
     <div className="App">
@@ -54,8 +81,10 @@ const App: React.FC = () => {
         }}
       />
       <button onClick={_ => addUser()}>登録</button>
-      <button onClick={_ => fetchUsers()}>fetch</button>
-      {mapUsers()}
+      <button onClick={_ => login()}>ログイン</button>
+      <button onClick={_ => post()}>投稿</button>
+      <button onClick={_ => del()}>削除</button>
+      {state.map((post, i) => <div key={i}><p>{post.title}</p><p>{post.content}</p><p>{post.delete ? "true" : "false"}</p></div>)}
     </div>
   );
 }
